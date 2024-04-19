737eff660de71fd17d597c86b2bfc3b09879443b
Sajal <sajal.moni@gmail.com>
2024-04-19 14:27:45 +0300

    game grid added

---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,7 @@
+const GameGrid = () => {
+  const [games, setGames] = useState([]);
+  const [error, setError] = useState('');
+  return <div>GameGrid</div>;
+};
+
+export default GameGrid;
---
+++ src/services/api-client.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+axios.create({
+  baseURL: 'https://api.rawg.io/api',
+  params: {
+    
+    key:'a23cd0bc33034dba94fdec9f3efbf000'
+  }
+});
