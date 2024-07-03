acccad05bd6e37c2ef606a12f961544fc5ed9817
Sajal <sajal.moni@gmail.com>
2024-07-03 14:03:31 +0300

    Fetch genres using react query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -24,7 +24,7 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
         Genres
       </Heading>
       <List>
-        {data.map((genre) => (
+        {data?.results.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,3 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 import genres from "../data/genres";
 
 export interface Genres {
@@ -6,6 +9,15 @@ export interface Genres {
   image_background: string;
 }
 
-const useGenres = () => ({data: genres, isLoading: false, error: null
+const useGenres = () => useQuery({
+queryKey:['genres'],
+queryFn:  () =>  
+  apiClient
+.get<FetchResponse<Genres>>('/genres')
+.then (response => response.data),
+staleTime: 1000 * 60 * 60 * 24, // 24 hours
+initialData: { count: genres.length, results: genres },
+
+
 })
 export default useGenres
