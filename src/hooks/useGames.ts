47cb609fc890384f5875b2d1d652aaafa12afbb0
Sajal <sajal.moni@gmail.com>
2024-07-08 13:59:44 +0300

    Refactor symplify time calculation

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
@@ -33,7 +34,7 @@ const useGames = (gameQuery: GameQuery) =>
 
         return lastPage.next ? allPages.length + 1: undefined;
       },
-      staleTime: 1000 * 60 * 60 * 24, // 24 hours
+      staleTime: ms('24h'), 
   })
   
 
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import APIClient from "../services/api-client";
 import genres from "../data/genres";
 
@@ -14,7 +15,7 @@ export interface Genres {
 const useGenres = () => useQuery({
 queryKey:['genres'],
 queryFn:  apiClient.getAll,
-staleTime: 1000 * 60 * 60 * 24, // 24 hours
+staleTime: ms('24h'), 
 initialData: genres,
 
 
