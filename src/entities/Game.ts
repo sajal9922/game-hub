abb7a6fbe003cc01678e94a4b89b21f7fbfddc40
Sajal <sajal.moni@gmail.com>
2024-07-12 16:36:36 +0300

    Build game atributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import { Genres } from "./Genres";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 
 
@@ -6,9 +8,10 @@ export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genres[];
   description_raw: string;
+  publishers: Publisher[];
   background_image: string;
-
   parent_platforms: { platform: Platform; }[];
   metacritic: number;
   rating_top: number;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,10 @@
+import { Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
-import useGame from '../hooks/useGame';
-import { Heading, Spinner, Text } from '@chakra-ui/react';
 import ExpandableText from '../components/ExpandableText';
+import useGame from '../hooks/useGame';
+import DefinitionItem from '../components/DefinitionItem';
+import CriticScore from '../components/CriticScore';
+import GameAtributes from '../components/GameAtributes';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -14,6 +17,7 @@ const GameDetailPage = () => {
     <>
       <Heading> {game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAtributes game={game} />
     </>
   );
 };
