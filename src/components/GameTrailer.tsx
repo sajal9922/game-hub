1e6ece2d0170dc9b07f606e0f317cec346a288f8
Sajal <sajal.moni@gmail.com>
2024-07-15 14:08:20 +0300

    build game screenshots

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -10,7 +10,7 @@ const GameTrailer = ({ gameId }: Props) => {
   if (isLoading) return null;
   if (error) throw error;
 
-  const first = data?.results[0];
+  const first = data?.results[2];
 
   return first ? (
     <video src={first.data[480]} poster={first.preview} controls />
